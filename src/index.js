b67d3a9ade372b2307e8f8640560fb4f1e1ce2fc
Chris Berry <bezzer@gmail.com>
2019-07-16 18:26:10 +1000

    Convert package and os to function

--- src/index.js
+++ src/index.js
@@ -1,81 +1,87 @@
 /**
- * Created by yeliex
+ * Created by yeliex, modified by bezzer
  */
 
-(function () {
-  const init = (function () {
+(function() {
+  const init = (function() {
     let OS = '';
-    const that = Object.assign({}, require("os"));
+    const that = Object.assign({}, require('os'));
     let platform = that.platform();
-    let isWechat = (typeof navigator === 'object' && navigator.userAgent && navigator.userAgent.match(/MicroMessenger/g));
-    let isElectron = (typeof navigator === 'object' && navigator.userAgent && navigator.userAgent.match(/Electron/g));
+    let isWechat =
+      typeof navigator === 'object' &&
+      navigator.userAgent &&
+      navigator.userAgent.match(/MicroMessenger/g);
+    let isElectron =
+      typeof navigator === 'object' &&
+      navigator.userAgent &&
+      navigator.userAgent.match(/Electron/g);
 
     if (global && global.nw) {
-      platform = "nw";
+      platform = 'nw';
     }
 
     switch (platform) {
-      case "browser":
-      case "nw":
-      {
-        // 浏览器时,正则判断US
-        OS = (function () {
-          var release = require("os").release();
+      case 'browser':
+      case 'nw': {
+        OS = (function() {
+          var release = require('os').release();
           if (release.match(/iPad|iPhone/g)) {
-            return "ios";
+            return 'ios';
           }
           if (release.match(/Macintosh|Mac OS X/g)) {
-            return "osx";
+            return 'osx';
           }
           if (release.match(/Windows/g)) {
-            return "win";
+            return 'win';
           }
           if (release.match(/Android/g)) {
-            return "android";
+            return 'android';
           }
           if (release.match(/Linux/g)) {
-            return "linux";
+            return 'linux';
           }
-          return "other";
-        }());
+          return 'other';
+        })();
         break;
       }
-      case "darwin":
-      {
-        OS = "osx";
+      case 'darwin': {
+        OS = 'osx';
         break;
       }
     }
-    that.isWin = (function () {
-      return OS === "win";
-    }());
-    that.isOSX = (function () {
-      return OS === "osx";
-    }());
-    that.isLinux = (function () {
-      return OS === "linux";
-    }());
-    that.isIOS = (function () {
-      return OS === "ios";
-    }());
-    that.isAndroid = (function () {
-      return OS === "android";
-    }());
-    that.isBrowser = (function () {
-      return platform === "browser" || platform === "nw" || isElectron || isWechat;
-    }());
-    that.isOnlyBrowser = (function () {
-      return platform === "browser" && !isWechat;
-    }());
-    that.isNW = (function () {
-      return platform === "nw";
-    }());
+    that.isWin = (function() {
+      return OS === 'win';
+    })();
+    that.isOSX = (function() {
+      return OS === 'osx';
+    })();
+    that.isLinux = (function() {
+      return OS === 'linux';
+    })();
+    that.isIOS = (function() {
+      return OS === 'ios';
+    })();
+    that.isAndroid = (function() {
+      return OS === 'android';
+    })();
+    that.isBrowser = (function() {
+      return (
+        platform === 'browser' || platform === 'nw' || isElectron || isWechat
+      );
+    })();
+    that.isOnlyBrowser = (function() {
+      return platform === 'browser' && !isWechat;
+    })();
+    that.isNW = (function() {
+      return platform === 'nw';
+    })();
     that.isElectron = isElectron;
     that.isWechat = isWechat;
-    that.os = OS;
-    that.platform = isWechat ? 'wechat' : isElectron ? 'Electron' : platform;
+    that.os = () => OS;
+    that.platform = () =>
+      isWechat ? 'wechat' : isElectron ? 'Electron' : platform;
     return that;
-  }());
+  })();
 
   if (typeof module === 'object') {
     module.exports = init;
@@ -83,4 +89,4 @@
   if (typeof navigator === 'object') {
     navigator.os = init;
   }
-}());
+})();
